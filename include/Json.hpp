edce4a6e5aadb900a42dc785b8568a221b3c0fb7
Francesco Pollarà <PolarFrancy@gmail.com>
2026-06-03 20:00:54 +0200

    json

---
+++ include/Json.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <Common.hpp>
+
+namespace json {
+
+  template<class T>
+  struct stringify;
+
+  template<class T>
+  concept object = requires (T candidate) {
+    { candidate.toJSONString() } -> convertible_to<string>;
+  } or requires (T candidate) {
+    stringify<T>{};
+    { stringify<T>{}(candidate) } -> convertible_to<string>;
+  };
+}
--- src/Main.cpp
+++ src/Main.cpp
@@ -4,6 +4,89 @@
 #include <TypeCheckerVisitor.hpp>
 #include <LLVMObjectEmitterVisitor.hpp>
 #include <LinkerVisitor.hpp>
+#include <Json.hpp>
+
+
+using namespace friday;
+
+template<>
+struct json::stringify<Variable> {
+  auto operator()(Variable const& self) -> string {
+    return "{}";
+  }
+};
+
+template<>
+struct json::stringify<Overload> {
+  auto operator()(Overload const& self) -> string {
+    return "{{\"type\": \"overload\", \"name\": \"{}\"}}"_f.format(
+      self.getQualifiedId()
+    );
+  }
+};
+
+template<>
+struct json::stringify<Struct> {
+  auto operator()(Struct const& self) -> string {
+    auto name = self.getQualifiedId();
+    auto symbols = self.getSymbols();
+
+    // TODO grammar must implement global variables
+    auto var2str = stringify<Variable>{};
+    auto ovl2str = stringify<Overload>{};
+
+    auto sym2str = [&](ISymbol* symbol) {
+      if(auto asVar = rtti::cast<Variable>(symbol)) 
+        return var2str(*asVar);
+      else if(auto asOverload = rtti::cast<Overload>(symbol)) 
+        return ovl2str(*asOverload);
+      else return "null"s;
+    };
+
+    return "{{\"type\": \"struct\", \"name\": \"{}\", \"symbols\": [{}]}}"_f.format(
+      name,
+      symbols 
+      | views::transform(sym2str)
+      | views::join_with(", "sv)
+      | ranges::to<string>()
+    );
+  }
+};
+
+
+
+template<>
+struct json::stringify<Namespace> {
+  auto operator()(Namespace const& self) -> string {
+    auto name = self.getQualifiedId();
+    auto symbols = self.getSymbols();
+
+    // TODO grammar must implement global variables
+    auto var2str = stringify<Variable>{};
+    auto sct2str = stringify<Struct>{};
+    auto ovl2str = stringify<Overload>{};
+
+    auto sym2str = [&](ISymbol* symbol) {
+      if(auto asVar = rtti::cast<Variable>(symbol)) 
+        return var2str(*asVar);
+      else if(auto asStruct = rtti::cast<Struct>(symbol)) 
+        return sct2str(*asStruct);
+      else if(auto asOverload = rtti::cast<Overload>(symbol)) 
+        return ovl2str(*asOverload);
+      else return "null"s;
+    };
+
+    return "{{\"type\": \"namespace\", \"name\": \"{}\", \"symbols\": [{}]}}"_f.format(
+      name,
+      symbols 
+      | views::transform(sym2str)
+      | views::join_with(", "sv)
+      | ranges::to<string>()
+    );
+  }
+};
+
+
 
 
 template<class Func>
@@ -14,7 +97,6 @@ auto measure(Func&& func, string label) -> string {
   return format("{{ \"label\": \"{}\", \"time\": \"{}\"}}", label, chrono::duration_cast<chrono::milliseconds>(end - begin));
 }
 
-using namespace friday;
 
 
 auto Main(vector<string_ref> paths) -> void {
@@ -39,6 +121,15 @@ auto Main(vector<string_ref> paths) -> void {
     ranges::for_each(discoveryErrors, SemanticError::report);
     return;
   }
+
+  auto toJSONString = views::values 
+  | views::transform(box<Namespace>::get);
+
+  for(auto nsp : context->namespaces | toJSONString) {
+    println("{}", json::stringify<Namespace>{}(*nsp));
+  }
+
+  println("{}", json::stringify<Namespace>{}(*context->global));
 
   // auto namespaceBindingErrors = NamespaceBindingVisitor{*context}.bind().errors();
 
